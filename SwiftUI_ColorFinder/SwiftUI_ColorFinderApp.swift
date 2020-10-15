c220b08bce45b3775d1735c19e06ec0db47e303c
Md Maruf Howlader <maruf.dhaka2010@gmail.com>
2020-10-15 18:27:03 +0600

    Initial Commit

---
+++ SwiftUI_ColorFinder/SwiftUI_ColorFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ColorFinderApp.swift
+//  SwiftUI_ColorFinder
+//
+//  Created by Maruf Howlader on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ColorFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
